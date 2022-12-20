46d7f4ef0bdc216e88483af962c746666c48c5b2
TejasIsAmazing <tejasisamazing.ntl@gmail.com>
2022-12-20 15:20:38 +0000

    Updated dashboard

--- static/script/dashboard/dashboard_input.js
+++ static/script/dashboard/dashboard_input.js
@@ -1,15 +1,28 @@
-// Testing code
 const input_button = document.getElementById('select-folder-btn');
 var directory_handles = undefined
-var all_files = {}
+var all_files = {
+    "file": [],
+    "directory": []
+}
+var current_viewed_folder = directory_handles
+var subfolder_view_path = []
 input_button.addEventListener('click', async () => {
-    all_files = {}
-    directory_handles = await window.showDirectoryPicker({mode:"readwrite"});
-    for await (thing of directory_handles.values()){
-        if (!(thing.kind in all_files)){
-            all_files[thing.kind] = []
-        }
-        all_files[thing.kind].push(thing)
+    all_files = {
+        "file": [],
+        "directory": []
     }
+    directory_handles = await window.showDirectoryPicker({mode:"readwrite"});
+    current_viewed_folder = directory_handles
+    subfolder_view_path = [current_viewed_folder]
+    for await (thing of directory_handles.values()){
+        if (thing.kind == "file") {
+            all_files[thing.kind].push(thing)
+        } else if (thing.kind == "directory") {
+            console.log(thing);
+            all_files[thing.kind].push(thing)
+        }
+    }
+    
+    console.log(all_files)
     update_view();
 });
